d2ea0274d9dc933421f911d750cf4110ba599cc4
Edward1217 <m932012@gmail.com>
2024-09-06 14:36:11 -0700

    frontend second commit

--- ClearSkin/src/App.jsx
+++ ClearSkin/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import Navbar from './components/Navbar.jsx'
+import SignUp from './pages/SignUp'; // Import the SignUp component
+import Home from './pages/Home.jsx';
+import SignIn from './pages/SignIn.jsx';
+import About from './pages/About.jsx';
+import Services from './pages/Services.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Navbar/>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/sign-in" element={<SignIn/>} />
+                <Route path="/about" element={<About />}/>
+                <Route path="/services" element={<Services />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
--- ClearSkin/src/main.jsx
+++ ClearSkin/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Import Bootstrap JavaScript
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
